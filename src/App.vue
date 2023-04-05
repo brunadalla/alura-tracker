3a6d7962034bc66055df3639c6a77050e54f7a03
brunadalla <bruna.dall9741@gmail.com>
2023-04-05 15:21:24 -0300

    feat: side section and form

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideSection />
+    </div>
+    <div class="column is-three-quarter">
+      <FormTask />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue"
+import SideSection from "./components/SideSection.vue"
+import FormTask from "./components/Form.vue"
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-});
+    SideSection,
+    FormTask,
+  },
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
