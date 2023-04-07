6c5b5d4829c52d2954fecfac3113a45bbed3bacb
brunadalla <bruna.dall9741@gmail.com>
2023-04-07 14:01:18 -0300

    feat: task component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
     </div>
     <div class="column is-three-quarter">
       <FormTask />
+      <div class="lista"></div>
+      <Task/>
     </div>
   </main>
 </template>
@@ -13,16 +15,20 @@
 import { defineComponent } from "vue"
 import SideSection from "./components/SideSection.vue"
 import FormTask from "./components/Form.vue"
+import Task from "./components/Task.vue"
 
 export default defineComponent({
   name: "App",
   components: {
     SideSection,
     FormTask,
+    Task,
   },
 })
 </script>
 
-<style></style>
-
-
+<style>
+.lista {
+  padding: 1.25rem;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">Description</div>
+      <div class="column">
+        <TimeFormatted :timeInSeconds="15" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import TimeFormatted from "./TimeFormatted.vue"
+
+export default defineComponent({
+  name: "TaskComponent",
+  components: {
+    TimeFormatted,
+  },
+})
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
